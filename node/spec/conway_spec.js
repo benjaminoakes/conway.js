3a58c8c5bd1ef8d263961acef723700ff1d70d0a
Benjamin Oakes <hello@benjaminoakes.com>
2015-07-20 13:42:07 -0500

    Refactor: Prefer single quotes

--- node/spec/conway_spec.js
+++ node/spec/conway_spec.js
@@ -1,8 +1,8 @@
-describe("Conway", function() {
+describe('Conway', function() {
   var Conway = require('../lib/conway');
 
   describe('Cell.next', function () {
-    it("is 1", function() {
+    it('is 1', function() {
       expect(Conway.Cell.next(3)).toEqual(1);
     });
   });
